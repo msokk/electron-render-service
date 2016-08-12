9d3d8148a6bb212c59aaddc62669d4c1d5a9950c
Mihkel Sokk <mihkelsokk@gmail.com>
2016-08-12 13:23:37 +0300

    Update util test

--- test/util.js
+++ test/util.js
@@ -5,7 +5,7 @@ const { printUsage } = require('../src/util');
 describe('util', () => {
   describe('#printUsage()', () => {
     it('should print default usage string', () => {
-      assert.equal(printUsage(), 'Usage: GET /[pdf|png|jpeg]?url=http://google.com&accessKey=[token]');
+      assert.equal(printUsage(), 'Usage: GET /[pdf|png|jpeg]?accessKey=[token]&url=http%3A%2F%2Fgoogle.com');
     });
   });
 });
