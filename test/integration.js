3a5a53b4773c25aa55d166c014e2e2e8c3c424fb
Mihkel Sokk <mihkelsokk@gmail.com>
2018-02-04 12:37:07 +0200

    xReformat test

--- test/integration.js
+++ test/integration.js
@@ -84,31 +84,31 @@ describe('integration', () => {
     });
   });
 
-    describe('POST /pdf', () => {
-      it('should render valid pdf from POSTED html in fixtures/example.html', function renderPdf(done) {
-        this.slow(10000);
-        const exampleHtmlPath = path.join(fixturePath, 'example.html');
+  describe('POST /pdf', () => {
+    it('should render valid pdf from POSTED html in fixtures/example.html', function renderPdf(done) {
+      this.slow(10000);
+      const exampleHtmlPath = path.join(fixturePath, 'example.html');
 
-        request.post('/pdf')
-          .parse(parseBuffer) // Superagent does not detect PDF
-          .type('form')
-          .query({ accessKey: process.env.RENDERER_ACCESS_KEY })
-          .send(fs.readFileSync(exampleHtmlPath, 'utf-8'))
-          .expect((res) => {
-              if (res.statusCode !== 200) {
-                  throw new Error(`Invalid response code: ${res.statusCode}\n${res.body}`);
-              }
+      request.post('/pdf')
+        .parse(parseBuffer) // Superagent does not detect PDF
+        .type('form')
+        .query({ accessKey: process.env.RENDERER_ACCESS_KEY })
+        .send(fs.readFileSync(exampleHtmlPath, 'utf-8'))
+        .expect((res) => {
+          if (res.statusCode !== 200) {
+            throw new Error(`Invalid response code: ${res.statusCode}\n${res.body}`);
+          }
 
-              const examplePdfPath = path.join(fixturePath, 'example.pdf');
-              const fixture = fs.readFileSync(examplePdfPath);
+          const examplePdfPath = path.join(fixturePath, 'example.pdf');
+          const fixture = fs.readFileSync(examplePdfPath);
 
-              if (res.body.slice(150).compare(fixture.slice(150)) === 0) return; // Slice out ModDate
+          if (res.body.slice(150).compare(fixture.slice(150)) === 0) return; // Slice out ModDate
 
-              fs.writeFileSync('./example_failed.pdf', res.body);
-              execSync('curl --upload-file ./example_failed.pdf https://transfer.sh/example_failed.pdf', { stdio: 'inherit' });
-              throw new Error(`${examplePdfPath} does not match rendered pdf`);
-          })
-          .end(done);
-        });
+          fs.writeFileSync('./example_failed.pdf', res.body);
+          execSync('curl --upload-file ./example_failed.pdf https://transfer.sh/example_failed.pdf', { stdio: 'inherit' });
+          throw new Error(`${examplePdfPath} does not match rendered pdf`);
+        })
+        .end(done);
     });
+  });
 });
--- test/stability.js
+++ test/stability.js
@@ -4,55 +4,54 @@ const fs = require('fs');
 const path = require('path');
 const async = require('async');
 const { execSync } = require('child_process');
+
 const bigTableHtml = fs.readFileSync(path.resolve(__dirname, 'fixtures/bigTable.html'), 'utf-8');
 
 const parseBuffer = (res, fn) => { // Superagent does not detect PDF
-	const data = [];
-	res.on('data', chunk => data.push(chunk));
-	res.on('end', () => fn(null, Buffer.concat(data)));
+  const data = [];
+  res.on('data', chunk => data.push(chunk));
+  res.on('end', () => fn(null, Buffer.concat(data)));
 };
 
 describe('stability', () => {
-	describe('POST /pdf', () => {
-		it('should render proper PDF\'s when posting large amounts of big HTML chunks', function (done) {
-			this.timeout(60000);
-			this.slow(30000);
+  describe('POST /pdf', () => {
+    it('should render proper PDF\'s when posting large amounts of big HTML chunks', function largeHTMLTest(done) {
+      this.timeout(60000);
+      this.slow(30000);
 
-			const fixturePath = path.join(__dirname, 'fixtures');
-			const examplePdfPath = path.join(fixturePath, 'bigTable.pdf');
-			const fixture = fs.readFileSync(examplePdfPath);
+      const fixturePath = path.join(__dirname, 'fixtures');
+      const examplePdfPath = path.join(fixturePath, 'bigTable.pdf');
+      const fixture = fs.readFileSync(examplePdfPath);
 
-			var q = async.queue(function(task, callback) {
-				request
-					.post('/pdf')
-					.parse(parseBuffer) // Superagent does not detect PDF
-					.type('form')
-					.send(bigTableHtml)
-					.query({ accessKey: process.env.RENDERER_ACCESS_KEY })
-					.expect((res) => {
-						if (res.statusCode !== 200) {
-							const errMsg = `Invalid response code: ${res.statusCode}\n${JSON.stringify(res.body)}`;
-							throw new Error(errMsg);
-						}
+      const q = async.queue((task, callback) => {
+        request
+          .post('/pdf')
+          .parse(parseBuffer) // Superagent does not detect PDF
+          .type('form')
+          .send(bigTableHtml)
+          .query({ accessKey: process.env.RENDERER_ACCESS_KEY })
+          .expect((res) => {
+            if (res.statusCode !== 200) {
+              const errMsg = `Invalid response code: ${res.statusCode}\n${JSON.stringify(res.body)}`;
+              throw new Error(errMsg);
+            }
 
-						if (res.body.slice(150).compare(fixture.slice(150)) === 0) return; // Slice out ModDate
+            if (res.body.slice(150).compare(fixture.slice(150)) === 0) return; // Slice out ModDate
 
-						fs.writeFileSync('./bigTable_failed.pdf', res.body);
-						execSync('curl --upload-file ./bigTable_failed.pdf https://transfer.sh/bigTable_failed.pdf', { stdio: 'inherit' });
-						throw new Error(`${examplePdfPath} (${task}) does not match rendered screenshot`);
-					})
-					.end(callback);
-			}, 8);
+            fs.writeFileSync('./bigTable_failed.pdf', res.body);
+            execSync('curl --upload-file ./bigTable_failed.pdf https://transfer.sh/bigTable_failed.pdf', { stdio: 'inherit' });
+            throw new Error(`${examplePdfPath} (${task}) does not match rendered screenshot`);
+          })
+          .end(callback);
+      }, 8);
 
-			for (var i = 0; i < 80; i++) {
-				q.push(i, function (err) {
-					if (err) {
-						done(err);
-					}
-				});
-			};
+      for (let i = 0; i < 80; i += 1) {
+        q.push(i, (err) => {
+          if (err) done(err);
+        });
+      }
 
-			q.drain = done;
-		});
-	});
+      q.drain = done;
+    });
+  });
 });
